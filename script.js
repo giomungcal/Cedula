4d4ea0fbe0605224653efcfb07a7cbe28e8f828f
Heherson Domael <h.domael@outlook.com>
2020-02-01 17:51:32 +0800

    Forms for each class type of applicants now accessible

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Mabuhay!");
+function selectClassType()
+{
+    var selectedClassType = document.getElementById("classtypelist").value, nextPage;
+
+    if (selectedClassType == "classa")
+        nextPage = "form_class_a.html";
+    else if (selectedClassType == "classab")
+        nextPage = "form_class_ab.html";
+    else if (selectedClassType == "classc")
+        nextPage = "form_class_c.html";
+    else if (selectedClassType == "select")
+        nextPage = "#";
+
+    document.querySelector("#chooseclasstype").setAttribute("action", nextPage);
+}
