41ae72916a10cca733147952f06afd0ed8beface
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-03-14 03:14:46 -0600

    squeaky clean

--- admin_class_a_table.php
+++ admin_class_a_table.php
@@ -1,37 +1,39 @@
 <thead>  
-                              <tr>  
-                                   <td>Date Processed</td>  
-                                   <td>Queue</td>  
-                                   <td>View</td>  
-                                   <td>First Name</td>  
-                                   <td>M.I.</td>  
-                                   <td>Last Name</td>  
-                                   <td>Gender</td>  
-                                   <td>Address</td>
-                              </tr>  
-                         </thead> 
-<?php  
-                         $con = mysqli_connect('localhost', 'root', '');
-                         mysqli_select_db($con, 'cedula');
-                         $query = "SELECT * FROM classa";
-                         $result=mysqli_query($con, $query);
+     <tr>  
+          <td>Date Processed</td>  
+          <td>Queue</td>  
+          <td>First Name</td>  
+          <td>M.I.</td>  
+          <td>Last Name</td>  
+          <td>Gender</td>  
+          <td>Address</td>
+          <td>View</td>  
+     </tr>  
+</thead> 
 
-                         while($row = mysqli_fetch_array($result))  
-                         {  
-                              echo '  
-                              <tr>  
-                                   <td>'.$row["dateProcessed"].'</td>
-                                   <td> A001-031120 </td>
-                                   <td><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⨭&nbsp;&nbsp;&nbsp;&nbsp;</a></td>
-                                   <td>'.$row["firstName"].'</td>  
-                                   <td>'.$row["middle"].'</td>  
-                                   <td>'.$row["lastName"].'</td>  
-                                   <td>'.$row["gender"].'</td>  
-                                   <td>'.$row["homeAddress"].'</td>   
-                              </tr>  
-                              ';  
-                         }  
-                         ?>  
+                         
+<?php  
+     $con = mysqli_connect('localhost', 'root', '');
+     mysqli_select_db($con, 'cedula');
+     $query = "SELECT * FROM classa";
+     $result=mysqli_query($con, $query);
+
+     while($row = mysqli_fetch_array($result))  
+     {  
+          echo '  
+          <tr>  
+               <td>'.$row["dateProcessed"].'</td>
+               <td> A001-031120 </td>
+               <td>'.$row["firstName"].'</td>  
+               <td>'.$row["middle"].'</td>  
+               <td>'.$row["lastName"].'</td>  
+               <td>'.$row["gender"].'</td>  
+               <td>'.$row["homeAddress"].'</td>   
+               <td><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;⨭&nbsp;&nbsp;&nbsp;&nbsp;</a></td>
+          </tr>  
+          ';  
+     }  
+?>  
 
 <script>  
 $(document).ready(function(){  
--- admin_class_ab_table.php
+++ admin_class_ab_table.php
@@ -1,39 +1,39 @@
 <thead>  
-                              <tr>  
-                                   <td>Date Processed</td>  
-                                   <td>First Name</td>  
-                                   <td>M.I.</td>  
-                                   <td>Last Name</td>  
-                                   <td>Tax Account Number</td>  
-                                   <td>ACR Number</td>
-                                   <td>Real Property Income</td>  
-                                   <td>Gross Receipts</td>  
-                                   <td>Salary</td>
-                              </tr>  
-                         </thead>  
-                         <?php  
-                         $con = mysqli_connect('localhost', 'root', '');
-                         mysqli_select_db($con, 'cedula');
-                         $query = "SELECT * FROM classab";
-                         $result=mysqli_query($con, $query);
+     <tr>  
+          <td>Date Processed</td>  
+          <td>First Name</td>  
+          <td>M.I.</td>  
+          <td>Last Name</td>  
+          <td>Tax Account Number</td>  
+          <td>ACR Number</td>
+          <td>Real Property Income</td>  
+          <td>Gross Receipts</td>  
+          <td>Salary</td>
+     </tr>  
+</thead>  
+<?php  
+     $con = mysqli_connect('localhost', 'root', '');
+     mysqli_select_db($con, 'cedula');
+     $query = "SELECT * FROM classab";
+     $result=mysqli_query($con, $query);
 
-                         while($row = mysqli_fetch_array($result))  
-                         {  
-                              echo '  
-                              <tr>  
-                                   <td>'.$row["dateProcessed"].'</td>
-                                   <td>'.$row["firstName"].'</td>  
-                                   <td>'.$row["middle"].'</td>  
-                                   <td>'.$row["lastName"].'</td>  
-                                   <td>'.$row["taxAccountNo"].'</td>  
-                                   <td>'.$row["ACRNo"].'</td>   
-                                   <td>'.$row["realPropertyIncome"].'</td>   
-                                   <td>'.$row["grossReceiptsFromBusiness"].'</td>   
-                                   <td>'.$row["salariesFromProfession"].'</td>   
-                              </tr>  
-                              ';  
-                         }  
-                         ?>
+     while($row = mysqli_fetch_array($result))  
+     {  
+          echo '  
+          <tr>  
+               <td>'.$row["dateProcessed"].'</td>
+               <td>'.$row["firstName"].'</td>  
+               <td>'.$row["middle"].'</td>  
+               <td>'.$row["lastName"].'</td>  
+               <td>'.$row["taxAccountNo"].'</td>  
+               <td>'.$row["ACRNo"].'</td>   
+               <td>'.$row["realPropertyIncome"].'</td>   
+               <td>'.$row["grossReceiptsFromBusiness"].'</td>   
+               <td>'.$row["salariesFromProfession"].'</td>   
+          </tr>  
+          ';  
+     }  
+?>
 
 <script>  
 $(document).ready(function(){  
