98d0cf723604d20b8937d6e8c99e46df53d68b6b
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-03-17 07:57:33 -0600

    HOLY FUCK DRAINED HELP
    help tab created with modulars

--- home.php
+++ home.php
@@ -28,7 +28,7 @@
                         <div class="logo"><a href=""><i>manila</i>&nbsp;<b>cedula</b></a></div>
                     <div class="menu">
                         <ul>
-                            <li id="btn1"><a class="btn"><b>How&nbsp;to&nbsp;Use</b></a></li>
+                            <li id="btn1"><a class="btn"><b>Help</b></a></li>
                             <!-- <li><a class="btn" href=""><b>Procedure</b></a></li> -->
                             <li><a class="btn" href=""><b>About</b></a></li>
                             <li><a class="btn" href=""><b>Settings</b></a></li> <!-- Change password feature. Yay or  nay? -H-->
@@ -61,13 +61,109 @@
                 </div>
                 <!-- The Modal -->
                 <div id="myModal" class="modal">
+                    <!-- Modal content -->
+                    <div class="modal-content-container">
+                        <span class="close">&times;&nbsp;&nbsp;</span>
+                        <div class="modal-content">
+                            <div class="icon"><img src="img/help/procedure.png" style="width:100px; height:100px;"></div>
+                            <div class="icon-title"><br><h1 style="">Procedure</h1><p class="icon-subtitle">Steps in Cedula Application</p></div>
+                            <br><br><br><br>
+                            <div class="steps-container">
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>1</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/choose.png"><h2>Choose class type</h2><p>Indicate which class you belong among the three types: <i><b>A</b>: Unemployed, <b>AB</b>: Employed, & <b>C</b>: Corporation</i></p></div>
+                                </div>
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>2</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/fill-up.png"><h2>Fill-up Form</h2><p>Provide all information needed in the form.</p></div>
+                                </div>
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>3</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/verify.png"><h2>Verify & Submit</h2><p>Make sure that all information is correct before pressing the submit button.</p></div>
+                                </div>
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>4</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/queue.png"><h2>Wait for Queue</h2><p>After submitting, applications will be forwarded to the CTC office for verification and processing.</p></div>
+                                </div>
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>5</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/present-reqs.png"><h2>Prepare and Present Requirements</h2><p>Upon being called, proceed to the designated window and present all requirements needed.</p></div>  
+                                </div>
+                                <div class="steps-content">
+                                    <div class="number-header"><h1>6</h1></div>
+                                    <div class="number-content"><img class="content-icons" src="img/help/pay.png"><h2>Pay</h2><p>After completing all these steps, you need to pay for your tax.</p></div>
+                                </div>
+                                </div>
 
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close">&times;</span>
-                    <p>Some text in the Modal..</p>
-                </div>
+                            <div class="icon"><img src="img/help/faq.png" style="width:100px; height:100px;"></div>
+                            <div class="icon-title"><br><h1 style="">FAQs</h1><p class="icon-subtitle">Frequently Asked Questions</p></div>
+                            <br>
+                            <div class="steps-container">
+                                    <div class="steps-content">
+                                        <!-- <div class="number-header"><h1></h1></div> -->
+                                        <div class="number-content"><h2>1. What are the requirements needed?</h2>
+                                            <div class="faqs-text">
+                                            <b>CLASS “A” </b>- For unemployed individuals 
+                                            <br>Requirements: 
+                                                <p>
+                                                    &emsp;- Information sheet<br>
+                                                    &emsp;- Valid ID <br>
+                                                    &emsp;- Fees/Payment<br>
+                                                </p>
+                                                <b>CLASS “AB” </b>- For employed individuals and for sole proprietor or a person who is the exclusive owner of a business.
+                                            <br>Requirements: 
+                                                <p>
+                                                    &emsp;- Information sheet<br>
+                                                    &emsp;- Valid ID <br>
+                                                    &emsp;- Proof of income (Form W2)<br>
+                                                    &emsp;- Fees/Payment<br>
+                                                </p>
+                                                <b>CLASS “C” </b>- For corporation.
+                                            <br>Requirements: 
+                                                <p>
+                                                    &emsp;- Information sheet<br>
+                                                    &emsp;- Valid ID <br>
+                                                    &emsp;- Appropriate Proof of income and/or assessment <br>
+                                                    &emsp;- Fees/Payment<br>
+                                                </p>
+                                            </div>
+                                        </div>
+                                        <br><br><br><br><br><br><br>
+                                        <div class="number-content"><h2>2. What Valid IDs are accepted?</h2>
+                                            &emsp;&emsp;✓ Philippine Passport<br>
+                                            &emsp;&emsp;✓ Driver’s License<br>
+                                            &emsp;&emsp;✓ SSS UMID Card<br>
+                                            &emsp;&emsp;✓ PhilHealth ID<br>
+                                            &emsp;&emsp;✓ TIN Card<br>
+                                            &emsp;&emsp;✓ Postal ID<br>
+                                            &emsp;&emsp;✓ Voter’s ID<br>
+                                            &emsp;&emsp;✓ PRC ID<br>
+                                            &emsp;&emsp;✓ Senior Citizen ID<br>
+                                            &emsp;&emsp;✓ OFW ID<br>
+                                        </div>
+                                        <br><br><br><br><br><br><br><br>
+                                        <div class="number-content"><h2>3. How do I compute my community tax?</h2>
+                                            <div class="faqs-text">
+                                            <b>CLASS “A”</b><br>
+                                                &emsp;Basic Community Tax = ₱10<br>
+                                            <br>
+                                            <b>CLASS “AB”</b><br>
+                                                &emsp;Basic Community Tax = ₱10<br>
+                                                &emsp;+ Gross Receipt or Earnings from Business during the preceding year P 1.00 for every P 1,000.00<br>
+                                                &emsp;+ Salaries or Gross Receipt or Earnings derived from exercise of Profession – P 1.00 for every P 1,000.00<br>
+                                                &emsp;+ Income from Real Property – P 1.00 for every P 1,000.00<br>
 
+                                            <b>CLASS “C”</b><br>
+                                                &emsp;Basic Community Tax – P 500.00<br>
+                                                &emsp;+ Assessed Value of Real Property owned in the Philippines. P2.00 for every P5, 000.00<br>
+                                                &emsp;+ Gross receipts including dividend earnings derived from business in the Philippines during the preceding year P2.00 for every P5, 000.00<br>
+                                            </div>
+                                        </div>
+                                    <!-- <div class="number-content"><h2>Fill-up Form</h2><p>Provide all information needed in the form.</p></div> -->
+                                    </div>
+                            </div>  
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
